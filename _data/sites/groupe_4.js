b0e46a3993a0546799a71164660c4273072efee6
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-11-14 16:55:45 +0100

    build : ajout de pages

--- _data/sites/groupe_4.js
+++ _data/sites/groupe_4.js
@@ -14,5 +14,13 @@ module.exports = {
         "https://coda-school.github.io/25-html-g4/themes/anime.html",
         "https://coda-school.github.io/25-html-g4/themes/choixthemes.html",
         "https://coda-school.github.io/25-html-g4/themes/pokemon.html",
+
+        "https://coda-school.github.io/25-html-g4/en/", // Ne pas oublier trailing slash
+        "https://coda-school.github.io/25-html-g4/en/accueil.html",
+        "https://coda-school.github.io/25-html-g4/en/themes/acteurs.html",
+        "https://coda-school.github.io/25-html-g4/en/themes/aie.html",
+        "https://coda-school.github.io/25-html-g4/en/themes/anime.html",
+        "https://coda-school.github.io/25-html-g4/en/themes/choixthemes.html",
+        "https://coda-school.github.io/25-html-g4/en/themes/pokemon.html",
     ]
 };
